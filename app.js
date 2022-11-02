b9ed4ecfb51e56668d5fcb28aca874a3be6130c2
aleSer308 <alegresergio@hotmail.com.ar>
2022-11-01 21:14:29 -0300

    first-commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("Express");
+const path = require('path');
+const app = express();
+
+let port = process.env.PORT || 3000
+
+app.listen(port, console.log("corriendo por puerto" + port) )
+
+app.use(express.static('public'));
+
+app.get("/", (req, res)=>{
+
+    res.sendFile(path.resolve('views/home.html'));
+
+});
+app.get("/login", (req, res)=>{
+
+    res.sendFile(path.resolve('views/login.html'));
+
+});
+app.get("/register", (req, res)=>{
+
+    res.sendFile(path.resolve('views/register.html'));
+
+});
