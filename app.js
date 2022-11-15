0fbda772477e3fc08b3ea75335b9fbce316af086
aleSer308 <alegresergio@hotmail.com.ar>
2022-11-15 18:43:12 -0300

    cambios app

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
+app.use(express.static('public'));
+
 let port = process.env.PORT || 3000
 
 app.listen(port, console.log("corriendo por puerto" + port) )
 
-app.use(express.static('public'));
 
 app.get("/", (req, res)=>{
 
